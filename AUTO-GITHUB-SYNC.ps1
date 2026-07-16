ef4dc5ab2ab4322c36c16d7a1a3d8bbf302af1cb
Ikhwan Munajat <ikhwanmunajat@users.noreply.github.com>
2026-07-16 23:19:37 +0700

    Auto sync IjatBilling 2026-07-16 23:19:37

---
+++ AUTO-GITHUB-SYNC.ps1
@@ -0,0 +1,10 @@
+﻿Set-Location "C:\IJATBILLING"
+
+git add -A
+
+git diff --cached --quiet
+if ($LASTEXITCODE -ne 0) {
+    $waktu = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    git commit -m "Auto sync IjatBilling $waktu"
+    git push origin main
+}
